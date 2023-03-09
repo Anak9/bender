87cff984294985bd0815e8f402a5b22dd9cf59e4
Sarah O Elias <sarahelias308@gmail.com>
2023-03-09 13:22:04 -0300

    Update bookings.js

--- public/js/bookings.js
+++ public/js/bookings.js
@@ -129,9 +129,8 @@ export const initBookingsEvents = () => {
 
     showCreditCardNumber();
 
-    console.log(teacherId, day.replaceAll('/', '-'), hour, modality, group);
     setTimeout(() => {
-      // bookClass(teacherId, day.replaceAll('/', '-'), hour, modality, group);
+      bookClass(teacherId, day.replaceAll('/', '-'), hour, modality, group);
     }, 6000);
   });
 };
